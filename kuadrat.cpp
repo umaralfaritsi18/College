e8d6e4bdc07dd974a61d29183aa331d0d047bb61
umaralfaritsi18 <umar.faritsi@binus.ac.id>
2024-09-07 13:19:29 +0700

    Tugas Menghitung Kuadrat

---
+++ kuadrat.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int nomor, kotak;
+
+    printf("Masukkan bilangan angka: ");
+    scanf("%d", &nomor);
+
+    kotak = nomor*nomor;
+    
+    printf("Kotak: %d\n", kotak);
+return 0;
+}
